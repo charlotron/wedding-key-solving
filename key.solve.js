741a22edc3c176219e7ec6971cf4ff372e71d825
Carlos Cenjor Ramirez <carlos.cenjor@sngular.com>
2022-05-23 00:15:49 +0200

    no message

--- key.solve.js
+++ key.solve.js
@@ -3,9 +3,7 @@
 const HEX_RADIX = 16;
 const HEX_COLORS = {
   RED: 'ff0000',
-  // PINK: 'ffc0cb',
   BLUE: '0000ff',
-  // BLACK: '000000',
 }
 const MS_IN_A_DAY = 1000 * 3600 * 24;
 const DECIMAL_DIGITS = 4;
